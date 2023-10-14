97b5c924711afcd8b708c9eec2f7badad39463ea
ro14hit <104704634+ro14hit@users.noreply.github.com>
2023-10-14 15:04:38 +0530

    Minor Fixes

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
+mongoose.connect()
 const app = express();
 
 app.listen(3000,()=>{
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { FaSearch } from 'react-icons/fa';
+import LocationSearchingRoundedIcon from '@mui/icons-material/LocationSearchingRounded';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
@@ -7,8 +7,8 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
-            <span className='text-slate-700'>Estate</span>
+            <span className='text-slate-500'>Estate</span>
+            <span className='text-slate-700'>Exchange</span>
           </h1>
         </Link>
         <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
@@ -17,7 +17,7 @@ export default function Header() {
             placeholder='Search...'
             className='bg-transparent focus:outline-none w-24 sm:w-64'
           />
-          <FaSearch className='text-slate-600' />
+          <LocationSearchingRoundedIcon className='text-slate-600' />
         </form>
         <ul className='flex gap-4'>
           <Link to='/'>
